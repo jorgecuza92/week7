b9f1e243dddf1677760b423af1be62679000cf91
Jorge Cuza <jorge.cuza92@gmail.com>
2021-03-18 15:41:33 -0500

    authentification through registration and login on trips application

--- day2/Assigment-Trips/server/app.js
+++ day2/Assigment-Trips/server/app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
 const session = require('express-session')
+const authenticate = require('../authentication/auth') // import authentication function
 const { v4: uuidv4 } = require('uuid');
+
+// function helloMiddleware(req,res,next) {
+//   console.log('Hello Middleware')
+//   next() // continue with original request 
+// }
+
 
 // setting up mustache as template engine
 app.engine('mustache', mustacheExpress())
@@ -19,25 +26,101 @@ app.use(session({
 }))
 
 
+// app.use(authenticate)
 let trips = []
 
-let users = [
-  {username: 'jacuza20', password: 'yomama'}
-]
+global.users = []
 
-app.post('/add-trip', (req,res) => {
+
+
+// allow user to register
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  if (req.session) {
+    req.session.username = username
+    req.session.password = password
+  }
+  // users.push(username) && users.push(password)
+  let user = { username: username, password: password }
+  users.push(user)
+
+  res.redirect('confirmation')
+})
+
+// login page rendering
+app.get('/login', (req, res) => {
+  res.render('login')
+
+})
+
+app.post('/login', (req, res) => {
+
+  const username = req.body.username
+  const password = req.body.password
+
+  const authenticatedUser = users.find((user) => {
+    return user.username == username && user.password == password
+
+  })
+  //if user login info is correct
+  if (authenticatedUser) {
+
+    //check if session is initialized
+    if (req.session) {
+      req.session.username = username
+
+    }
+
+    res.redirect('/add-trip')
+
+  } else { // if the username or password is incorrect redirect to login again
+    res.render('login', { message: 'Incorrect password or username. Please re-enter credentials.' })
+  }
+
+
+
+})
+
+
+// confirmation page after user registraiton
+app.get('/confirmation', (req, res) => {
+  res.render('confirmation', { getUsers: users })
+})
+
+
+
+// add trip page
+app.get('/add-trip',authenticate, (req, res) => {
+  res.render('add-trip')
+})
+
+
+app.post('/add-trip', (req, res) => {
   const tripImage = req.body.tripImage
   const destination = req.body.destination
   const departure = req.body.departure
   const arrival = req.body.arrival
-  
-  let trip = {tripImage: tripImage, destination: destination, departure: departure, arrival: arrival, taskId: uuidv4()}
+
+  let trip = { tripImage: tripImage, destination: destination, departure: departure, arrival: arrival, taskId: uuidv4() }
   trips.push(trip)
 
   res.redirect('/trips')
 })
 
-app.post('/delete-task', (req,res) => {
+
+// renders page of trips added
+app.get('/trips',authenticate, (req, res) => {
+  res.render('trips', { allTrips: trips })
+})
+
+// deletion
+app.post('/delete-task', (req, res) => {
   const taskId = req.body.taskId
 
   trips = trips.filter((trip) => {
@@ -48,49 +131,6 @@ app.post('/delete-task', (req,res) => {
 
 })
 
-app.get('/add-trip', (req,res) => {
-  res.render('add-trip')
-})
-
-
-app.get('/trips', (req,res) => {
-  res.render('trips', {allTrips: trips})
-})
-
-
-// login page rendering
-app.get('/login', (req,res) => {
-  res.render('login')
-
-})
-
-app.post('/login', (req,res) => {
-
-  const username = req.body.username
-  const password = req.body.password
-
-  const authenticatedUser = users.find((user) => {
-    return user.username == username && user.password == password
-
-  })
-  //if user login info is correct
-  if(authenticatedUser) {
-
-    //check if session is initialized
-    if(req.session) {
-      req.session.username = username
-
-    }
-    
-    res.redirect('/add-trip')
-
-  } else { // if the username or password is incorrect redirect to login again
-      res.render('login', {message: 'Incorrect password or username. Please re-enter credentials.'})
-  }
-
-
-
-})
 
 
 app.listen(3000, () => {
